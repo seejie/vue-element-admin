ec21dd6adafb0ef98bdf9152cb725a384cce298d
liusijie02 <liusijie02@meituan.com>
2021-03-19 13:23:35 +0800

    todo

--- src/pages/plan/monthly-apply.vue
+++ src/pages/plan/monthly-apply.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="wrapper">
+    <el-form :model="form" inline>
+      <el-form-item label="申请单号" label-width="72px">
+        <el-input v-model="form.order" placeholder="请输入" style="width: 195px;" />
+      </el-form-item>
+
+      <el-form-item label="申请人" label-width="72px">
+        <el-input v-model="form.user" placeholder="请输入" style="width: 195px;" />
+      </el-form-item>
+
+      <el-form-item label="部门" label-width="72px">
+        <dept-select :data.sync="form.dept" />
+      </el-form-item>
+
+      <el-form-item label="申请月份" label-width="72px">
+        <el-date-picker v-model="form.month" type="month" placeholder="选择月份" style="width: 100%;" />
+      </el-form-item>
+
+      <el-form-item label="使用渠道" label-width="72px">
+        <channel-select :data="form.channel" />
+      </el-form-item>
+
+      <el-form-item label="用途" label-width="72px">
+        <program-select :data="form.program" />
+      </el-form-item>
+    </el-form>
+
+    <el-form inline>
+      <el-form-item label="选择单片" label-width="72px">
+        <products-select :data="form.products" />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="add">添加计划外SKU</el-button>
+        <el-button type="primary" @click="apply">剩余量全部申请</el-button>
+        <el-button type="primary" @click="submit">提交月度申请</el-button>
+      </el-form-item>
+    </el-form>
+
     <el-table :data="tableData" border>
       <el-table-column type="selection" width="55" />
       <el-table-column fixed prop="date" label="缩略图" header-align="center" />
@@ -22,11 +60,29 @@
 </template>
 
 <script>
+import deptSelect from '../components/dept-select.vue'
+import ChannelSelect from '../components/channel-select.vue'
+import ProgramSelect from '../components/program-select.vue'
+import ProductsSelect from '../components/products-select.vue'
+
 export default {
   name: 'MonthlyApply',
+  components: {
+    deptSelect,
+    ChannelSelect,
+    ProgramSelect,
+    ProductsSelect
+  },
   data() {
     return {
-      tableData: []
+      tableData: [],
+      form: {
+        order: '',
+        user: '',
+        dept: '',
+        month: ''
+      },
+      products: ''
     }
   },
   mounted() {
@@ -38,6 +94,12 @@ export default {
     },
     edit(item) {
       console.log(item, '-----item-----')
+    },
+    add() {
+
+    },
+    apply() {
+
     }
   }
 }
